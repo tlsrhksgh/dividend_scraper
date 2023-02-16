a8b96665f4598bc00a3bbca9673ce46117f17f01
tlsrhksgh <sin99145@naver.com>
2023-02-16 22:38:48 +0900

    Feat: yahoo finance 배당금과 배당금 지급 일자(년,월,일) 파싱

--- src/main/java/com/single/project/scraper/ScraperApplication.java
+++ src/main/java/com/single/project/scraper/ScraperApplication.java
@@ -1,13 +1,53 @@
 package com.single.project.scraper;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-@SpringBootApplication
+import java.io.IOException;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+//@SpringBootApplication
 public class ScraperApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ScraperApplication.class, args);
+
+//        SpringApplication.run(ScraperApplication.class, args);
+
+        try {
+            String url = "https://finance.yahoo.com/quote/COKE/history?period1=187920000&period2=1676505600&interval=capitalGain%7Cdiv%7Csplit&filter=div&frequency=1mo";
+            Connection connection = Jsoup.connect(url);
+            Document document = connection.get();
+
+            Elements elements = document.getElementsByAttributeValue("data-test", "historical-prices");
+            Element tableElement = elements.get(0);
+
+            Element tbody = tableElement.children().get(1);
+
+            for (Element e : tbody.children()) {
+                String txt = e.text();
+                System.out.println(txt);
+                if(!txt.endsWith("Dividend")) {
+                    continue;
+                }
+
+                String[] splits = txt.split(" ");
+                String month = splits[0];
+                int day = Integer.parseInt(splits[1].replace(",", ""));
+                int year = Integer.parseInt(splits[2]);
+                String dividend = splits[3];
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 }
